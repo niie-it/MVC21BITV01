3975053975812323149df6ff88cd62517fc13b6d
HIENLTH 2023 <hyhien@gmail.com>
2024-06-21 10:52:32 +0700

    Day 03 - Demo aync

---
+++ DemoAndLabs/DemoAndLabs/Controllers/DemoController.cs
@@ -0,0 +1,34 @@
+﻿using DemoAndLabs.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DemoAndLabs.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult SyncDemo()
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            MyDemo.FuncA();
+            MyDemo.FuncB();
+            MyDemo.FuncC();
+            sw.Stop();
+
+            return Content($"Chạy hết {sw.ElapsedMilliseconds} ms");
+        }
+
+        public async Task<IActionResult> AsyncDemo()
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            var x = MyDemo.FuncAAsync();
+            var y = MyDemo.FuncBAsync();
+            var z = MyDemo.FuncCAsync();
+            await x; await y; await z;
+            sw.Stop();
+
+            return Content($"Chạy hết {sw.ElapsedMilliseconds} ms");
+        }
+    }
+}

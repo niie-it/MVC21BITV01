bc8fe95b6ca98665cddedc69d27bd64cf5a7d2c3
HIENLTH 2023 <hyhien@gmail.com>
2024-06-21 09:41:51 +0700

    Lab02.02

---
+++ DemoAndLab/DemoAndLab/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using DemoAndLab.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoAndLab.Controllers
+{
+    public class ProductController : Controller
+    {
+        static List<Product> products = new List<Product>
+        {
+            new Product { Id = 1, Name="Iphone 13", Price=22909000},
+            new Product { Id = 2, Name="Iphone 15", Price=2999000},
+            new Product { Id = 3, Name="SS Galaxy S24", Price=2899000}
+        };
+        public IActionResult Index()
+        {
+            return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            products.Add(product);
+
+            return View("Index", products);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ﻿# MVC21BITV01
 
-## Demo trên lớp
+## Demo trên lớp buổi 03 (21/06/2024)
+
+* Phân biệt Sync vs Async
+* Lab02 - CRUD Product

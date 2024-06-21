ad8e42bd4a86a5a4fb33ff107dcaa1f3dbd75e3c
HIENLTH 2023 <hyhien@gmail.com>
2024-06-21 07:10:24 +0700

    Buoi 02 - Truyen nhan du lieu

---
+++ TruyenNhanDuLieu/TruyenNhanDuLieu/Controllers/DemoController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TruyenNhanDuLieu.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult NhapThongTin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult KetQua(string HoTen, int NamSinh)
+        {
+            //Gửi dữ liệu qua View để hiển thị
+            ViewBag.HoTen = HoTen;
+            ViewBag.NamSinh = NamSinh;
+
+            return View();
+        }
+    }
+}

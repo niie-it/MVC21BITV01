ce216f669b7353889b12530fd662ea5d4582e902
HIENLTH 2023 <hyhien@gmail.com>
2024-06-22 11:13:06 +0700

    Demo upload file

---
+++ Lab03/Lab03/Controllers/FileUploadController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab03.Controllers
+{
+	public class FileUploadController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult UploadFile(IFormFile file)
+		{
+			//Lưu file vô thư mục Data của wwwroot
+			var fullPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Data", file.FileName);
+
+			using (var myfile = new FileStream(fullPath, FileMode.CreateNew))
+			{
+				file.CopyTo(myfile);
+			}
+
+			return RedirectToAction("Index");
+		}
+
+
+        [HttpPost]
+        public IActionResult UploadFiles(List<IFormFile> files)
+        {
+			foreach (var file in files)
+			{
+				//Lưu file vô thư mục Data của wwwroot
+				var fullPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Data", file.FileName);
+
+				using (var myfile = new FileStream(fullPath, FileMode.CreateNew))
+				{
+					file.CopyTo(myfile);
+				}
+			}
+
+            return RedirectToAction("Index");
+        }
+    }
+}

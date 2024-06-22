beba35133769e14405332a24bcb02c7eb21986b8
HIENLTH 2023 <hyhien@gmail.com>
2024-06-22 12:06:45 +0700

    Demo student

---
+++ Lab03/Lab03/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using Lab03.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab03.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Manage(StudentInfo model, string savebutton)
+        {
+            return View();
+        }
+
+        public IActionResult ShowInfo(string type)
+        {
+            return View();
+        }
+    }
+}
---
+++ Lab03/Lab03/Models/StudentInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab03.Models
+{
+    public class StudentInfo
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public double Mark { get; set; }
+
+    }
+}

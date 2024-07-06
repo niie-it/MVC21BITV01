37e7c3d1f4e5ec7f65b4f76c6e2fe1ed7059efc9
HIENLTH 2023 <hyhien@gmail.com>
2024-07-06 12:07:45 +0700

    jQueryValidation

---
+++ DemojQueryValidate/DemojQueryValidate/Controllers/DemoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemojQueryValidate.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string CheckExistedEmail(string Email)
+        {
+            var dsEmail = new List<string>
+            {
+                "admin@admin.com", "teo@teo.co"
+            };
+            return dsEmail.Contains(Email) ? "true" : "false";
+        }
+    }
+}

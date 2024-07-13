097fdeb520cc8f00dd18161dda19e462171c36ed
HIENLTH 2023 <hyhien@gmail.com>
2024-07-13 11:19:22 +0700

    Layout customer

---
+++ L01WebApp/L01WebApp/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace L01WebApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
---
+++ L01WebApp/L01WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L01WebApp.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}

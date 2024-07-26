2d7456d1455b71c674e78645e28da424663d019c
HIENLTH 2023 <hyhien@gmail.com>
2024-07-26 09:34:40 +0700

    CR product

---
+++ Lab07/Lab07/Controllers/Products.cs
@@ -0,0 +1,40 @@
+﻿using Lab07.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab07.Controllers
+{
+	public class Products : Controller
+	{
+		private readonly MvcNiieLabContext _context;
+
+		public Products(MvcNiieLabContext context)
+		{
+			_context = context;
+		}
+
+		public IActionResult Index()
+		{
+			var data = _context.Products.Include(p => p.Category).Include(sp => sp.Supplier);
+			return View(data);
+		}
+
+		#region Create Product
+		[HttpGet]
+		public IActionResult Create()
+		{
+			ViewBag.CategoryId = new SelectList(_context.Categories.ToList(), "Id", "NameVN");
+			ViewBag.SupplierId = new SelectList(_context.Suppliers.ToList(), "Id", "Name");
+
+            return View();
+		}
+
+		[HttpPost]
+		public IActionResult Create(Product model)
+		{
+			return View();
+		}
+		#endregion
+	}
+}

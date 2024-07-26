bc12d42ace61aca0bac562273c87c37f9acd312e
HIENLTH 2023 <hyhien@gmail.com>
2024-07-26 07:33:02 +0700

    Code day 12

---
+++ Lab07/Lab07/Controllers/SuppilersController.cs
@@ -0,0 +1,23 @@
+﻿using Lab07.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab07.Controllers
+{
+	public class SuppilersController : Controller
+	{
+		private readonly MvcNiieLabContext _context;
+
+		public SuppilersController(MvcNiieLabContext context)
+		{
+			_context = context;
+		}
+
+		public IActionResult Index()
+		{
+			var data = _context.Suppliers != null ? _context.Suppliers.ToList() : new List<Supplier>();
+			return View(data);
+
+		}
+	}
+}
